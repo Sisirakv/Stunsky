7743442e2808a45ed269f377fe5c27426865a512
Sisira kv <sisirakv1998@gmail.com>
2022-11-14 15:34:54 +0530

    first commit

---
+++ web/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(requset):
+    return render(requset, 'index.html')
+
+def about_us(requset):
+    return render(requset, 'about-us.html')

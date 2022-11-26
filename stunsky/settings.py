affcf332a7b960a50e8da43c09d669ea87be70d2
Sisira kv <sisirakv1998@gmail.com>
2022-11-26 14:37:36 +0530

    complete with out testimonial

--- stunsky/settings.py
+++ stunsky/settings.py
@@ -144,19 +144,3 @@ STATIC_ROOT = BASE_DIR / "assets"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_USE_TLS = True
-# EMAIL_USE_SSL = False
-# EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER ='sisuthej@gmail.com'
-# EMAIL_HOST_PASSWORD = "ogsymmaeducxicry"
-
-
-# EMAIL_BACKEND = config('EMAIL_BACKEND')
-# EMAIL_HOST = config('EMAIL_HOST')
-# EMAIL_PORT = config('EMAIL_PORT')
-# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-# EMAIL_USE_TLS = True
-# RECIPIENT_ADDRESS = config('RECIPIENT_ADDRESS')
